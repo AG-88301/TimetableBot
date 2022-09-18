32fcfb56cf901b8d6769a40c7dc7ed08cc6252a0
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2022-09-18 14:57:33 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -27,38 +27,35 @@ async def set_timetable(ctx: commands.Context, *args) -> None:
     elif len(ctx.message.attachments) != 0:
         img = ctx.message.attachments[0]
         await img.save("img.jpg")
-        Timetable("img.jpg").get_timetable()
+        ttbl_handler.get_timetable()
         
     else:
         await ctx.send("Looks like you haven't attached an image")
         
 @client.command()
 async def timetable(ctx: commands.Context, user="default") -> None:
-    x = int(str(user)[2:-1])
-    print(x)
-    author = str(ctx.message.author) if user == "default" else str(ctx.message.author)
-    print(author)
+    usr = str(ctx.message.author) if user == "default" else str(ctx.message.mentions[0]._user)
     
-    ttbl = db_handler.get(author, "timetable")
-    ttbl = ttbl_handler.expand(ttbl)
+    if db_handler.find(usr, "timetable"):
+        ttbl = db_handler.get(usr, "timetable")
+        ttbl = ttbl_handler.expand(ttbl)
+        
+        image = ttbl_handler.table_img(ttbl)
+        if db_handler.find(usr, "color"):
+            image = ttbl_handler.add_mask(image, tuple(db_handler.get(usr, "color")))
+        
+        with io.BytesIO() as image_binary:
+            image.save(image_binary, 'PNG')
+            image_binary.seek(0)
+            await ctx.send(file=discord.File(fp=image_binary, filename='image.png'))
     
-    image = ttbl_handler.table_img(ttbl)
-    if db_handler.find(author, "color"):
-        image = ttbl_handler.add_mask(image, tuple(db_handler.get(author, "color")))
-    
-    with io.BytesIO() as image_binary:
-        image.save(image_binary, 'PNG')
-        image_binary.seek(0)
-        await ctx.send(file=discord.File(fp=image_binary, filename='image.png'))
+    else:
+        await ctx.send("Oops, looks like {} set up a timetable yet.".format("you haven't" if user == "default" else (usr[:-5] + " hasn't")))
 
 @client.command()
 async def timetable_color(ctx: commands.Context, r: str, g: str, b: str) -> None:
     db_handler.add(str(ctx.message.author), ["color", (int(r), int(g), int(b))])
     await ctx.send("Saved!")
-    
-@client.command()
-async def next(ctx:commands.Context):
-    pass
     
 client.remove_command('help')
 @client.command()
@@ -71,4 +68,4 @@ async def help(ctx: commands.Context) -> None:
     
     await ctx.send(embed=embed)
 
-client.run("TOKEN >:(")
+client.run("TOKEN :D")
